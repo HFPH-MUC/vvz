5c0ea3cf8fb11c4e9e22608d44d0d3fee63c5169
Uwe Stange <uwe.stange@gmail.com>
2022-07-19 13:26:14 +0200

    Rename app

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hfph-nuxt-buefy',
+    title: 'HFPH',
     htmlAttrs: {
       lang: 'en'
     },
