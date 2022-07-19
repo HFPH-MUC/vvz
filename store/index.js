6c1ecc091153de4b042a3cdd729881c2871316cb
Uwe Stange <uwe.stange@gmail.com>
2022-07-19 13:33:55 +0200

    Lint store/index.js

--- store/index.js
+++ store/index.js
@@ -1,29 +1,29 @@
 // https://nuxtjs.org/guide/vuex-store
 
 export const state = () => ({
-  courses: {}
+  courses: []
 })
 
 export const actions = {
-  async fetchCourses(state) {
+  async fetchCourses (state) {
     // make request
-    let req = await this.$axios.$get('/a5-vlvz-api')
+    const req = await this.$axios.$get('/a5-vlvz-api')
       .then((data) => {
         console.log(data)
       })
-    state.courses = req.data;
-    return req.data;
+    state.courses = req.data
+    return req.data
   }
 }
 
 export const mutations = {
-  increment(state) {
+  increment (state) {
     state.counter++
   }
 }
 
 export const getters = {
-  getCounter(state) {
+  getCounter (state) {
     return state.counter
   }
 }
