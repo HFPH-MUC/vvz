64c1b068e476e6c7a95188322427441176633718
Uwe Stange <uwe.stange@gmail.com>
2022-08-05 16:20:17 +0200

    Fontawesome styles.css missing in prod

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,19 +46,19 @@
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
           <div class="flex justify-center pt-4 pb-4 space-x-2 is-size-4">
             <a href="https://www.hfph.de/" target="_blank" class="mr-4">
-              <font-awesome-icon icon="arrow-up-right-from-square" size="xs" />
+              <font-awesome-icon icon="arrow-up-right-from-square" />
             </a>
             <a href="https://www.facebook.com/hfph.muenchen/" target="_blank" class="mr-4">
-              <font-awesome-icon :icon="['fab', 'facebook']" size="xs" />
+              <font-awesome-icon :icon="['fab', 'facebook']" />
             </a>
             <a href="https://twitter.com/hfph_muc" target="_blank" class="mr-4">
-              <font-awesome-icon :icon="['fab', 'twitter']" size="xs" />
+              <font-awesome-icon :icon="['fab', 'twitter']" />
             </a>
             <a href="https://www.instagram.com/hfphmuc/" target="_blank" class="mr-4">
-              <font-awesome-icon :icon="['fab', 'instagram']" size="xs" />
+              <font-awesome-icon :icon="['fab', 'instagram']" />
             </a>
             <a href="https://www.linkedin.com/school/15094016" target="_blank" class="mr-4">
-              <font-awesome-icon :icon="['fab', 'linkedin']" size="xs" />
+              <font-awesome-icon :icon="['fab', 'linkedin']" />
             </a>
           </div>
         </div>
@@ -93,5 +93,9 @@ export default {
   .navbar.is-hfph-gelb .navbar-brand > .navbar-item,
   .navbar.is-hfph-gelb .navbar-brand .navbar-link {
     color: $hfph-blau;
+  }
+
+  a svg {
+    height: inherit;
   }
 </style>
